590825e0a01f642c4d601412fbfbeca9e6a087c6
Wiktor <wiktor.rostkowski@hotmail.com>
2024-04-25 22:55:53 +0200

    Junit test add

--- BMI-Plus/app/src/androidTest/java/com/s23141/bmiplus/ExampleInstrumentedTest.java
+++ BMI-Plus/app/src/androidTest/java/com/s23141/bmiplus/ExampleInstrumentedTest.java
@@ -9,6 +9,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import com.s23141.bmiplus.ui.bmi.BMIFragment;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +19,28 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class s23141InstrumentedTest {
+public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.s23141.bmiplus", appContext.getPackageName());
     }
+
+    @Test
+    public void CountBMITest()
+    {
+        //Given
+        int height =150;
+        double weight = 75.4;
+        //When
+        String res;
+        res = BMIFragment.calculate(height,weight);
+        //Then
+        assertEquals("33.51111111111111",res);
+
+
+    }
+
+
 }
